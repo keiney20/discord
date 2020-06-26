27597c3deec728b4bd7fae1f1a80756fce39c311
keiney20 <67480115+keiney20@users.noreply.github.com>
2020-06-26 20:16:27 +0200

    Add files via upload

---
+++ database/gifs.js
@@ -0,0 +1,62 @@
+/*
+  Author: Keiney
+  Website: keiney.com
+  Version: 0.0.8
+*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+module.exports = {
+
+boobs: function(){
+
+    var gifs =
+    [
+        "https://here.set-your-gif-link.com",
+    ]
+
+    var select = Math.floor(Math.random() * gifs.length);
+      return gifs[select];
+},
+
+    blowjobs: function(){
+
+        var gifs =
+    [
+        "https://here.set-your-gif-link.com",
+    ]
+    var select = Math.floor(Math.random() * gifs.length);
+    return gifs[select];
+},
+
+   asses: function(){
+
+        var gifs =
+    [
+        "https://here.set-your-gif-link.com",
+    ]
+    var select = Math.floor(Math.random() * gifs.length);
+    return gifs[select];
+},
+
+   hentai: function(){
+
+        var gifs =
+    [
+        "https://here.set-your-gif-link.com",
+    ]
+    var select = Math.floor(Math.random() * gifs.length);
+    return gifs[select];
+},
+
+pussys: function(){
+
+        var gifs =
+    [
+        "https://here.set-your-gif-link.com",
+    ]
+    var select = Math.floor(Math.random() * gifs.length);
+    return gifs[select];
+}
+}
